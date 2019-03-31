befc10661945ba01c4bab5ab2f6cac157d30cb0a
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 17:28:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,20 @@
 var chocolateBars = ["snickers", "hundrad grand", "kitkat", "skittles"]
 chocolateBars.unshift("hersheys")
 console.log(chocolateBars[0])
+
+Function addElementToBeginningOfArray(arrayA,ElementA){
+  return [ElementA,...arrayA]
+  
+}
+
+Function destructivelyAddElementToBeginningOfArray(arrayA,ElementA){
+  return arrayA=[ElementA,...arrayA]
+}
+
+Function addElementToEndOfArray(arrayA,ElementA){
+  
+}
+
+Function destructivelyAddElementToEndOfArray(arrayA,ElementA){
+  
+}
